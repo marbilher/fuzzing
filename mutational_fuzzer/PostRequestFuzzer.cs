89e97f6e9f219bfc6263e0ac865535a8d2c57981
marbilher <marlonbilbaohernandez@gmail.com>
2021-01-02 16:38:06 -0800

    post fuzzer

---
+++ mutational_fuzzer/PostRequestFuzzer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace mutational_fuzzer
+{
+    public class PostRequestFuzzer
+    {
+        public static void MakeRequest(string[] args)
+        {
+            string[] requestLines = File.ReadAllLines(args[0]);
+            string[] parms = requestLines[requestLines.Length - 1].Split('&');
+            string host = string.Empty;
+            StringBuilder requestBuilder = new StringBuilder();
+
+            foreach (string ln in requestLines)
+            {
+                if (ln.StartsWith("Host:"))
+                    host = ln.Split(' ')[1].Replace("\r", string.Empty);
+                requestBuilder.Append(ln + "\n");
+            }
+            //https://stackoverflow.com/questions/6686261/what-at-the-bare-minimum-is-required-for-an-http-request
+            string request = requestBuilder.ToString() + "\r\n";    //for http requests, must end in \r\n or else will hang
+            Console.WriteLine(request);
+
+            IPEndPoint rhost = new IPEndPoint(IPAddress.Parse(host), 80);
+            foreach (string parm in parms)
+            {
+                using (Socket sock = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp))
+                {
+                    sock.Connect(rhost);
+
+                    string val = parm.Split('=')[1];
+                    string req = request.Replace("=" + val, "=" + val + "'");
+
+                    byte[] reqBytes = Encoding.ASCII.GetBytes(req);
+                    sock.Send(reqBytes);
+
+                    byte[] buf = new byte[sock.ReceiveBufferSize];
+
+                    sock.Receive(buf);
+                    string response = Encoding.ASCII.GetString(buf);
+                    if (response.Contains("error in your SQL syntax"))
+                        Console.WriteLine("Parameter " + parm + "might be vulnerable.");
+                    Console.WriteLine(" to SQL injection with value: " + val + "'");
+                    
+                }
+            }
+
+        }
+    }
+}
--- mutational_fuzzer/Program.cs
+++ mutational_fuzzer/Program.cs
@@ -8,40 +8,43 @@ namespace mutational_fuzzer
     {
         public static void Main(string[] args)
         {
-            string url = args[0];
-            int index = url.IndexOf("?");
-            string[] parms = url.Remove(0, index + 1).Split("&");
-            foreach (string parm in parms)
-            {
-                string xssUrl = url.Replace(parm, parm + "df<xss>df");
-                string sqlUrl = url.Replace(parm, parm + "sdfd'sa");
+            Console.WriteLine("Starting");
 
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sqlUrl);
-                request.Method = "GET";
+            PostRequestFuzzer.MakeRequest(args);
+            //string url = args[0];
+            //int index = url.IndexOf("?");
+            //string[] parms = url.Remove(0, index + 1).Split("&");
+            //foreach (string parm in parms)
+            //{
+            //    string xssUrl = url.Replace(parm, parm + "df<xss>df");
+            //    string sqlUrl = url.Replace(parm, parm + "sdfd'sa");
 
-                string sqlresp = string.Empty;
-                Console.WriteLine("Reading first streams..");
-                using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream()))
-                    sqlresp = rdr.ReadToEnd();
+            //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(sqlUrl);
+            //    request.Method = "GET";
 
-                request = (HttpWebRequest)WebRequest.Create(xssUrl);
-                request.Method = "GET";
-                string xssresp = string.Empty;
-                Console.WriteLine("Reading second stream..");
-                using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream()))
-                    xssresp = rdr.ReadToEnd();
+            //    string sqlresp = string.Empty;
+            //    Console.WriteLine("Reading first streams..");
+            //    using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream()))
+            //        sqlresp = rdr.ReadToEnd();
 
-                if (xssresp.Contains("<xss>"))
-                {
-                    Console.WriteLine("Possible XSS point in " + parm);
-                }
-                if (sqlresp.Contains("error in your SQL syntax"))
-                {
-                    Console.WriteLine("SQL injection point found in " + parm);
-                }
-                Console.WriteLine("Finished..");
+            //    request = (HttpWebRequest)WebRequest.Create(xssUrl);
+            //    request.Method = "GET";
+            //    string xssresp = string.Empty;
+            //    Console.WriteLine("Reading second stream..");
+            //    using (StreamReader rdr = new StreamReader(request.GetResponse().GetResponseStream()))
+            //        xssresp = rdr.ReadToEnd();
 
-            }
+            //    if (xssresp.Contains("<xss>"))
+            //    {
+            //        Console.WriteLine("Possible XSS point in " + parm);
+            //    }
+            //    if (sqlresp.Contains("error in your SQL syntax"))
+            //    {
+            //        Console.WriteLine("SQL injection point found in " + parm);
+            //    }
+            //    Console.WriteLine("Finished..");
+
+            //}
         }
     }
 }
